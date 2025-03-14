af18d7cc9d5651890729ef9409ff82d0b96bf0ae
tejas <tejasgowrish@gmail.com>
2025-03-14 09:39:29 +0530

    initial

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main(void) {
+  cout << "TEJAS GOWRISH";
+  cout << "PES1UG22CS650";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
